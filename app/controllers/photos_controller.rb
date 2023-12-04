45988b1f3ef4f51ef6247fa9afc336e38cc593be
dinerograham <102576939+dinerograham@users.noreply.github.com>
2023-12-04 00:03:16 +0000

    destroy and create images

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,8 +4,44 @@ class PhotosController < ApplicationController
     matching_photos = Photo.all
 
     @list_of_photos = matching_photos.order({:created_at => "desc"})
-
     render({:template => "photo_templates/index"})
+  end
+
+  def show
+    url_id = params.fetch("path_id")
+
+    matching_images = Photo.where({:id => url_id})
+
+    @the_photo = matching_images[0]
+    render({:template => "photo_templates/show"})
+  end
+
+  def destroy
+    the_id = params.fetch("terminate_id")
+
+    matching_images = Photo.where({:id => the_id})
+
+    the_image = matching_images[0]
+
+    the_image.destroy
+    
+    redirect_to("/photos")
+  end
+
+  def create
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+    input_owner_id = params.fetch("query_owner_id")
+
+    a_new_photo = Photo.new
+    a_new_photo.image = input_image
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id
+
+    a_new_photo.save
+    #render({:template => "photo_templates/create"})
+
+    redirect_to("/photos/" + a_new_photo.id.to_s)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   get("/users/:username", {:controller => "users", :action => "show"})
 
   get("/photos", {:controller => "photos", :action => "index"})
+
+  get("/photos/:path_id", {:controller => "photos", :action => "show"})
+
+  get("/delete_photo/:terminate_id", {:controller => "photos", :action => "destroy"})
+
+  get("/insert_photo", {:controller => "photos", :action=> "create"})
   
 end
